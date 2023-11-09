1afe9c2cd95a6a6f9b2c8f2f1feb1a38d98dedef
Yurii Rybachok <drummer2009@ukr.net>
2023-11-09 11:03:43 +0200

    record voice & display him

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,47 @@
 <script setup>
+import { ref, onMounted } from 'vue';
+
+const transcript = ref('');
+const isRecording = ref(false);
+
+const Recognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const sr = new Recognition();
+
+onMounted(() => {
+  sr.continuous = true;
+  sr.interimResults = true;
+
+  sr.onstart = () => {
+    console.log('SR Started');
+    isRecording.value = true;
+  };
+  sr.onend = () => {
+    console.log('SR Stopped');
+    isRecording.value = false;
+  };
+  sr.onresult = (evt) => {
+    console.log(evt.results);
+    const t = Array.from(evt.results)
+      .map((result) => result[0])
+      .map((result) => result.transcript)
+      .join('');
+    transcript.value = t;
+  };
+});
+
+const ToggleMic = () => {
+  if (isRecording.value) {
+    sr.stop();
+  } else {
+    sr.start();
+  }
+};
 </script>
 
 <template>
   <div class="app">
-  
+    <button :class="`mic`" @click="ToggleMic">Record</button>
+    <div class="transcript" v-text="transcript"></div>
   </div>
 </template>
 
@@ -17,6 +55,6 @@
 
 body {
   background: #281936;
-  color: #FFF;
+  color: #fff;
 }
 </style>
