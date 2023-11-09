412b371914f01975f2411ab4e18a9a63af41e5f0
Yurii Rybachok <drummer2009@ukr.net>
2023-11-09 09:36:24 +0200

    setup project

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+</script>
+
+<template>
+  <div class="app">
+  
+  </div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Fira Sans', sans-serif;
+}
+
+body {
+  background: #281936;
+  color: #FFF;
+}
+</style>
